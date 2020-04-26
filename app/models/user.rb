4c71615cb7fe50cbd10d13c0e0b7a2bc7f7930a2
zeyadsaleh <zeyadmostafasaleh@gmail.com>
2020-04-26 02:30:54 +0200

    only missing the hash Crypt bug in then lab

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,39 @@
+require 'bcrypt'
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable 
+    include BCrypt
     attr_accessor :password
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
     validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
     validates :password, :confirmation => true #password_confirmation attr
-    # validates_length_of :password, :in => 4..20, :on => :create
+    # validates_length_of :password, :in => 6...20, :on => :create
     before_save :encrypt_password
+	after_save :clear_password
+	def encrypt_password
+	  if password.present?
+	    # self.salt = BCrypt::Engine.generate_salt
+	    # self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
+	    self.encrypted_password = BCrypt::Password.create(password) if password.present?
+	  end
+	end
+	def clear_password
+	  self.password = nil
+	end
 
-    after_save :clear_password
-    def encrypt_password
-        if password.present?
-            self.salt = BCrypt::Engine.generate_salt
-            self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
-        end
-    end
-    def clear_password
-        self.password = nil
-    end
+	def self.authenticate(username_or_email="", login_password="")
+	  if  EMAIL_REGEX.match(username_or_email)
+	    user = User.find_by_email(username_or_email)
+	  else
+	    user = User.find_by_username(username_or_email)
+	  end
+	  if user && user.match_password(login_password)
+	    return user
+	  else
+	    return false
+	  end
+	end
+	def match_password(login_password="")
+	#   self.encrypted_password == BCrypt::Engine.hash_secret(l	, self.salt)
+	  BCrypt::Password.new(encrypted_password) == login_password
+	end
 end
