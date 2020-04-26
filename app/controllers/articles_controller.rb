a8b7d003735bc1acaeb55f814076e55d318248a0
zeyadsaleh <46134907+zeyadsaleh@users.noreply.github.com>
2020-04-26 23:02:07 +0200

    un comment brfore_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
      
   # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   # @articles = Article.accessible_by(current_ability)
   load_and_authorize_resource
 
