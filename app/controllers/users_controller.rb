631e86d89ffa3c180bb8dba7e97e0bbee700a532
zeyadsaleh <zeyadmostafasaleh@gmail.com>
2020-04-26 12:04:09 +0200

    finishing up part 2 of the lab without any bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(users_params)
     if @user.save
+      # abort @user.inspect
       flash[:notice] = "You signed up successfully"
       flash[:color]= "valid"
+      redirect_to articles_path
     else
       flash[:notice] = "Form is invalid"
       flash[:color]= "invalid"
+      render "new"
     end
-    render "new"
   end
 
   private
       def users_params
-        params.require(:user).permit(:username, :encrypted_password, :email, :salt)
+        params.require(:user).permit(:username, :password, :email, :password_confirmation)
       end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 require 'bcrypt'
 class User < ApplicationRecord
     include BCrypt
-    attr_accessor :password
+    attr_accessor :password, :password_confirmation
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
     validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
     validates :password, :confirmation => true #password_confirmation attr
-    # validates_length_of :password, :in => 6...20, :on => :create
+    validates_length_of :password, :in => 6...20, :on => :create
     before_save :encrypt_password
 	after_save :clear_password
 	def encrypt_password
 	  if password.present?
-	    # self.salt = BCrypt::Engine.generate_salt
-	    # self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
-	    self.encrypted_password = BCrypt::Password.create(password) if password.present?
+	    self.salt = BCrypt::Engine.generate_salt
+	    self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
+	    # self.encrypted_password = BCrypt::Password.create(password) 
 	  end
 	end
 	def clear_password
